12b7a4c339e868b33ca576286c774034690a4018
yykdev <assavictory@gmail.com>
2019-09-17 22:01:48 +0900

    schema.graphql , graph.d.ts 빌드

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type Chat {\n  id: Int!\n  message: [Message]\n  participants: [User]\n  createdAt: String!\n  updatedAt: String!\n}\n\ntype Message {\n  id: Int!\n  text: String!\n  chat: Message!\n  user: User!\n  createdAt: String!\n  updatedAt: String!\n}\n\ntype Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  isFav: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n\ntype Ride {\n  id: Int!\n  status: String!\n  pickUpAddress: String!\n  pickUpLat: Float!\n  pickUpLng: Float!\n  dropOffAddress: String!\n  dropOffLat: Float!\n  dropOffLng: Float!\n  price: Float!\n  distance: String!\n  duration: String!\n  createdAt: String!\n  updatedAt: String!\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Query {\n  user: User\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
+export const typeDefs = ["type Chat {\n  id: Int!\n  messages: [Message]\n  participants: [User]\n  createdAt: String!\n  updatedAt: String!\n}\n\ntype Message {\n  id: Int!\n  text: String!\n  chat: Chat!\n  user: User!\n  createdAt: String!\n  updatedAt: String!\n}\n\ntype Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  isFav: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n\ntype Ride {\n  id: Int!\n  status: String!\n  pickUpAddress: String!\n  pickUpLat: Float!\n  pickUpLng: Float!\n  dropOffAddress: String!\n  dropOffLat: Float!\n  dropOffLng: Float!\n  price: Float!\n  distance: String!\n  duration: String!\n  passenger: User!\n  driver: User!\n  createdAt: String!\n  updatedAt: String!\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n  chat: Chat\n  messages: [Message]\n  verifications: [Verification]\n  ridesAsPassenger: [Ride]\n  ridesAsDriver: [Ride]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Query {\n  user: User\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  user: User!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -23,13 +23,18 @@ export interface User {
   lastLng: number | null;
   lastLat: number | null;
   lastOrientation: number | null;
+  chat: Chat | null;
+  messages: Array<Message> | null;
+  verifications: Array<Verification> | null;
+  ridesAsPassenger: Array<Ride> | null;
+  ridesAsDriver: Array<Ride> | null;
   createdAt: string;
   updatedAt: string | null;
 }
 
 export interface Chat {
   id: number;
-  message: Array<Message> | null;
+  messages: Array<Message> | null;
   participants: Array<User> | null;
   createdAt: string;
   updatedAt: string;
@@ -38,19 +43,19 @@ export interface Chat {
 export interface Message {
   id: number;
   text: string;
-  chat: Message;
+  chat: Chat;
   user: User;
   createdAt: string;
   updatedAt: string;
 }
 
-export interface Place {
+export interface Verification {
   id: number;
-  name: string;
-  lat: number;
-  lng: number;
-  address: string;
-  isFav: boolean;
+  target: string;
+  payload: string;
+  key: string;
+  used: boolean;
+  user: User;
   createdAt: string;
   updatedAt: string;
 }
@@ -67,16 +72,19 @@ export interface Ride {
   price: number;
   distance: string;
   duration: string;
+  passenger: User;
+  driver: User;
   createdAt: string;
   updatedAt: string;
 }
 
-export interface Verification {
+export interface Place {
   id: number;
-  target: string;
-  payload: string;
-  key: string;
-  used: boolean;
+  name: string;
+  lat: number;
+  lng: number;
+  address: string;
+  isFav: boolean;
   createdAt: string;
   updatedAt: string;
 }
