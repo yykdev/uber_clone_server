cefc2c3c02d0717b1d567d6fee370079b98d2e3b
yykdev <assavictory@gmail.com>
2019-09-18 23:58:28 +0900

    이메일 회원가입

---
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -0,0 +1,42 @@
+import { Resolvers } from "../../../types/resolvers";
+import {
+    EmailSignUpResponse,
+    EmailSignUpMutationArgs
+} from "../../../types/graph";
+import User from "../../../entities/User";
+
+const resolvers: Resolvers = {
+    Mutation: {
+        EmailSignUp: async (
+            _,
+            args: EmailSignUpMutationArgs
+        ): Promise<EmailSignUpResponse> => {
+            const { email } = args;
+            try {
+                const existingUser = await User.findOne({ email });
+                if (existingUser) {
+                    return {
+                        ok: false,
+                        error: "You should log in instead",
+                        token: null
+                    };
+                } else {
+                    await User.create({ ...args }).save();
+                    return {
+                        ok: true,
+                        error: null,
+                        token: "Coming Soon!"
+                    };
+                }
+            } catch (error) {
+                return {
+                    ok: false,
+                    error: error.message,
+                    token: null
+                };
+            }
+        }
+    }
+};
+
+export default resolvers;
