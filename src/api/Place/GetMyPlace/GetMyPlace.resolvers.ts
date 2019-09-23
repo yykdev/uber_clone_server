7adf932b40c4fc15811c6aff9164e8d1f81de956
yykdev <assavictory@gmail.com>
2019-09-23 18:38:08 +0900

    사용자의 목적지 장소 조회 기능

---
+++ src/api/Place/GetMyPlace/GetMyPlace.resolvers.ts
@@ -0,0 +1,41 @@
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import { GetMyPlaceResponse } from "../../../types/graph";
+import User from "../../../entities/User";
+
+const resolvers: Resolvers = {
+    Query: {
+        GetMyPlace: privateResolver(
+            async (_, __, { req }): Promise<GetMyPlaceResponse> => {
+                try {
+                    const user = await User.findOne(
+                        { id: req.user.id },
+                        { relations: ["places"] }
+                    );
+
+                    if (user) {
+                        return {
+                            ok: true,
+                            places: user.places,
+                            error: null
+                        };
+                    } else {
+                        return {
+                            ok: false,
+                            places: null,
+                            error: "User not found"
+                        };
+                    }
+                } catch (error) {
+                    return {
+                        ok: false,
+                        places: null,
+                        error: error.message
+                    };
+                }
+            }
+        )
+    }
+};
+
+export default resolvers;
