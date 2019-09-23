1abbc0bcab2244d1a9a6460f76838bfd63e3c38c
yykdev <assavictory@gmail.com>
2019-09-23 16:43:19 +0900

    장소 수정, 삭제 기능 구현

---
+++ src/api/Place/EditPlace/EditPlace.resolvers.ts
@@ -0,0 +1,55 @@
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import User from "../../../entities/User";
+import { EditPlaceMutationArgs, EditPlaceResponse } from "../../../types/graph";
+import Place from "../../../entities/Place";
+import cleanNullArgs from "../../../utils/cleanNullArgs";
+
+const resolver: Resolvers = {
+    Mutation: {
+        EditPlace: privateResolver(
+            async (
+                _,
+                args: EditPlaceMutationArgs,
+                { req }
+            ): Promise<EditPlaceResponse> => {
+                const user: User = req.user;
+
+                try {
+                    const place = await Place.findOne({ id: args.placeId });
+
+                    if (place) {
+                        if (place.userId === user.id) {
+                            const notNull = cleanNullArgs(args);
+
+                            await Place.update(
+                                { id: args.placeId },
+                                { ...notNull }
+                            );
+
+                            return {
+                                ok: true,
+                                error: null
+                            };
+                        } else {
+                            return {
+                                ok: false,
+                                error: "Not Authorized"
+                            };
+                        }
+                    } else {
+                        return {
+                            ok: false,
+                            error: "Place not found"
+                        };
+                    }
+                } catch (error) {
+                    return {
+                        ok: false,
+                        error: error.message
+                    };
+                }
+            }
+        )
+    }
+};
