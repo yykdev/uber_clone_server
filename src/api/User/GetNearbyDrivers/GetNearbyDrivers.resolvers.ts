909d6110e6937766b98525f6800d0ac004a6fb72
yykdev <assavictory@gmail.com>
2019-09-24 14:42:54 +0900

    GetNearbyDrivers - 근처 드라이버 정보 조회 기능 구현

---
+++ src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
@@ -0,0 +1,37 @@
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import { GetNearbyDriversResponse } from "../../../types/graph";
+import User from "../../../entities/User";
+import { Between, getRepository } from "typeorm";
+
+const resolvers: Resolvers = {
+    Query: {
+        GetNearbyDrivers: privateResolver(
+            async (_, __, { req }): Promise<GetNearbyDriversResponse> => {
+                const user: User = req.user;
+                const { lastLat, lastLng } = user;
+                try {
+                    const drivers: User[] = await getRepository(User).find({
+                        isDriving: true,
+                        lastLat: Between(lastLat - 0.05, lastLng + 0.05),
+                        lastLng: Between(lastLng - 0.05, lastLng + 0.05)
+                    });
+
+                    return {
+                        ok: true,
+                        error: null,
+                        drivers
+                    };
+                } catch (error) {
+                    return {
+                        ok: false,
+                        error: error.message,
+                        drivers: null
+                    };
+                }
+            }
+        )
+    }
+};
+
+export default resolvers;
