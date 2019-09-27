1d8610cde22025b7557fc9eddae141dd75ac3ec8
yykdev <assavictory@gmail.com>
2019-09-27 16:02:32 +0900

    드라이버 주변 운행요청 조회 1건 출력으로 로직 변경

--- src/api/Ride/GetNearbyRides/GetNearbyRides.resolvers.ts
+++ src/api/Ride/GetNearbyRides/GetNearbyRides.resolvers.ts
@@ -15,29 +15,37 @@ const resolvers: Resolvers = {
                     const { lastLat, lastLng } = user;
 
                     try {
-                        const rides = await getRepository(Ride).find({
+                        const ride = await getRepository(Ride).findOne({
                             status: "REQUESTING",
                             pickUpLat: Between(lastLat - 0.05, lastLat + 0.05),
                             pickUpLng: Between(lastLng - 0.05, lastLng + 0.05)
                         });
 
-                        return {
-                            ok: true,
-                            error: null,
-                            rides
-                        };
+                        if (ride) {
+                            return {
+                                ok: true,
+                                error: null,
+                                ride
+                            };
+                        } else {
+                            return {
+                                ok: true,
+                                error: null,
+                                ride: null
+                            };
+                        }
                     } catch (error) {
                         return {
                             ok: false,
                             error: error.messave,
-                            rides: null
+                            ride: null
                         };
                     }
                 } else {
                     return {
                         ok: false,
                         error: "You are not a driver",
-                        rides: null
+                        ride: null
                     };
                 }
             }
