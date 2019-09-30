c9b0dca9394537e73216fbfbb5f4f597d205c058
yykdev <assavictory@gmail.com>
2019-09-30 18:09:14 +0900

    운행승인(ACCEPTED) 상태로 업데이트 될 경우 채팅 방 생성

--- src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
+++ src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -6,6 +6,7 @@ import {
     UpdateRideStatusResponse
 } from "../../../types/graph";
 import Ride from "../../../entities/Ride";
+import Chat from "../../../entities/Chat";
 
 const resolvers: Resolvers = {
     Mutation: {
@@ -20,14 +21,22 @@ const resolvers: Resolvers = {
                     try {
                         let ride: Ride | undefined;
                         if (args.status === "ACCEPTED") {
-                            ride = await Ride.findOne({
-                                id: args.rideID,
-                                status: "REQUESTING"
-                            });
+                            ride = await Ride.findOne(
+                                {
+                                    id: args.rideID,
+                                    status: "REQUESTING"
+                                },
+                                { relations: ["passenger"] }
+                            );
                             if (ride) {
                                 ride.driver = user;
                                 user.isTaken = true;
                                 user.save();
+
+                                await Chat.create({
+                                    driver: user,
+                                    passenger: ride.passenger
+                                }).save();
                             }
                         } else {
                             ride = await Ride.findOne({
