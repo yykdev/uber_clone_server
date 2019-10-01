da23a7975a6913200dc7bfa2a609957852be9629
yykdev <assavictory@gmail.com>
2019-10-01 12:21:24 +0900

    채팅방 생성시 chatId 운행 요청서 정보에 추가

--- src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
+++ src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -33,10 +33,13 @@ const resolvers: Resolvers = {
                                 user.isTaken = true;
                                 user.save();
 
-                                await Chat.create({
+                                const chat = await Chat.create({
                                     driver: user,
                                     passenger: ride.passenger
                                 }).save();
+
+                                ride.chat = chat;
+                                ride.save();
                             }
                         } else {
                             ride = await Ride.findOne({
