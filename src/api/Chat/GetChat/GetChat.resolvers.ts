0b8218ee8a16b740a151293b958deef64372fbf8
yykdev <assavictory@gmail.com>
2019-10-01 12:20:27 +0900

    채팅방 정보 조회시 메시지 내용 리스트 릴레이션 추가

--- src/api/Chat/GetChat/GetChat.resolvers.ts
+++ src/api/Chat/GetChat/GetChat.resolvers.ts
@@ -15,9 +15,12 @@ const resolvers: Resolvers = {
                 const user: User = req.user;
 
                 try {
-                    const chat = await Chat.findOne({
-                        id: args.chatId
-                    });
+                    const chat = await Chat.findOne(
+                        {
+                            id: args.chatId
+                        },
+                        { relations: ["messages"] }
+                    );
 
                     if (chat) {
                         if (
